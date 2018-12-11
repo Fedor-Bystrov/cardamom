25d5f9dcbf2840445a3c17594f7ba2e63e8ae80b
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-12 00:20:48 +0300

    use javalin instead of akka-http

--- src/main/java/me/fedorbystrov/syncer/App.java
+++ src/main/java/me/fedorbystrov/syncer/App.java
@@ -1,7 +1,10 @@
 package me.fedorbystrov.syncer;
 
+import io.javalin.Javalin;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("works");
+    final var app = Javalin.create().start(7000);
+    app.get("/", ctx -> ctx.result("Hello World"));
   }
 }
