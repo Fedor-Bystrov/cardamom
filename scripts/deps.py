31ca5f266f97e808d7f00283d946dd58e4360208
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-13 22:55:42 +0300

    add project.yaml and yaml parsing method

--- scripts/deps.py
+++ scripts/deps.py
@@ -1,4 +1,6 @@
 import subprocess
+import requests
+import yaml
 
 deps_path = '/Users/fbystrov/.tyr/deps'
 path = 'http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar'
@@ -8,10 +10,19 @@ def spawn_wget(url: str) -> int:
     result = subprocess.run(['wget', '-P', deps_path, url])
     return result.returncode
 
-spawn_wget(path)
-
+# spawn_wget(path)
 
 # maven api guide:
 # https://search.maven.org/classic/#api
 def search_maven():
     prefix = 'http://search.maven.org/#search|ga|1|guice'
+
+
+with open('project.yaml', 'r') as f:
+    deps = yaml.load(f)
+    print('project: ', deps['project'])
+    print('deps: ', deps['deps'])
+    
+def get_deps() -> list:
+    with open('project.yaml', 'r') as f:
+        return yaml.load(f)['deps']
