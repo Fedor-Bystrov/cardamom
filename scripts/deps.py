e5b85b2b07e227642e3690b8eb646d41797aef3f
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-13 22:10:36 +0300

    spawn wget to download jar file

--- scripts/deps.py
+++ scripts/deps.py
@@ -1,5 +1,17 @@
-import requests
+import subprocess
 
-response = requests.get('https://httpbin.org/ip')
+deps_path = '/Users/fbystrov/.tyr/deps'
+path = 'http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar'
 
-print('Your IP is {0}'.format(response.json()['origin']))
+
+def spawn_wget(url: str) -> int:
+    result = subprocess.run(['wget', '-P', deps_path, url])
+    return result.returncode
+
+spawn_wget(path)
+
+
+# maven api guide:
+# https://search.maven.org/classic/#api
+def search_maven():
+    prefix = 'http://search.maven.org/#search|ga|1|guice'
