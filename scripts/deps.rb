4147e6be34d8c9e63f4690083d8de877247a1dca
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-14 17:18:15 +0300

    add read_project_file method

--- scripts/deps.rb
+++ scripts/deps.rb
@@ -1,5 +1,19 @@
+require 'yaml'
+
+# Runs wget in a new subprocess
+# Params:
+# +url+:: URL of file
 def spawn_wget(url)
   IO.popen("wget -P ~/.tyr/deps #{url}") { |f| puts f.gets}
 end
 
-spawn_wget('http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar')
+# Reads a project configuration file project.yaml
+# located at the root of project folder
+# Returns hash with project config
+def read_project_file
+  return YAML.load_file('project.yaml')
+end
+
+
+#puts read_project_file['deps']
+#spawn_wget('http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar')
