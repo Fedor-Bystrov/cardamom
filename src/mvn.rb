68dd393e6f91ea24518bebcec98c85b6acd04ba9
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-15 19:11:54 +0300

    add get_latest_version mvn.rb

--- src/mvn.rb
+++ src/mvn.rb
@@ -1,10 +1,12 @@
 require 'bundler/setup'
 require 'nokogiri'
 require 'faraday'
+require 'json'
 
 module MVN
   @@srch_conn = Faraday.new(:url => 'https://search.maven.org')
-  @@remote_content = '/remotecontent?filepath=%s'
+  @@remotecontent_uri = '/remotecontent?filepath=%s'
+  @@select_uri = '/solrsearch/select?q=g:"%s"+AND+a:"%s"&rows=1&wt=json'
 
   # Fetches pom.xml from maven repository for given dependency
   # Returns pom object for given dependency
@@ -13,6 +15,17 @@ module MVN
   def self.fetch_pom(dep)
     res = @@srch_conn.get @@remote_content % dep
     return POM.new res.body
+  end
+
+  # Finds latest version for dependency with provided
+  # groupdId and artifactiId
+  # Params:
+  # +groupId+:: dependency groupId
+  # +artifactId+:: dependency artifactId
+  def self.get_latest_version(groupId, artifactId)
+    res = @@srch_conn.get @@select_uri % [groupId, artifactId]
+    res_json = JSON::parse(res.body).fetch('response')
+    return res_json.fetch('docs')[0].fetch('latestVersion')
   end
 
   class POM
@@ -24,3 +37,4 @@ module MVN
 end
 
 # MVN::fetch_pom('com/sparkjava/spark-core/2.7.2/spark-core-2.7.2.pom')
+# MVN::get_latest_version('javax.servlet', 'javax.servlet-api')
