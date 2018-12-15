860c14c15488ba95199a7caa6605b9156ba22510
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-15 22:27:14 +0300

    add todos

--- src/deps.rb
+++ src/deps.rb
@@ -46,3 +46,11 @@ puts project_deps
 # dependencies.each {|dep| puts $pom_filepath_prefix + dep}
 # fetch_pom 'com/sparkjava/spark-core/2.7.2/spark-core-2.7.2.pom'
 # spawn_wget('http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.25/slf4j-simple-1.7.25.jar')
+
+# 1. Собираем все зависимости, резолвим версии если не указаны
+#    Зависимости из project.yml -> зависимости зависемостей из project.yml и т.д
+#    В итоге должно получится (дерево или сет?) зависимостей
+# 2. Скачиваем зависимости
+# 3. Сохраняем результать в project.lock (например как в Pipfile.lock, посмотреть на yarn.lock)
+# 4. Не плохо бы проверять хэши
+# 5. Нужно ли хранить список модулей их имена и то чтот они экспортят, реквайрят?
