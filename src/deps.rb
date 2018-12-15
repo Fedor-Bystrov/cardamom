2be3b6476ec22ef69c513280a6752e7cd7264acc
Fedor Bystrov <augustbystrov@gmail.com>
2018-12-15 20:48:27 +0300

    add POM class to mvn.rb

--- src/deps.rb
+++ src/deps.rb
@@ -4,35 +4,13 @@ require 'faraday'
 require 'yaml'
 require 'set'
 
-$deps_path = '~/.tyr/deps'
-$filepath_pattern = /^(.+):(.+)@(.+)$/
-$pom_filepath_prefix = 'https://search.maven.org/remotecontent?filepath=%s'
-$pom_param_pattern = /^\$\{(.+)\}$/
-$mvn_search_url = 'https://search.maven.org/solrsearch/select?q=g:"%s" AND a:"%s"&rows=1&wt=json'
-
-
-def spawn_wget(url)
-
-end
+$deps_path = '~/.cardamom/deps'
 
 # Reads a project configuration file project.yaml
 # located at the root of project folder
 # Returns hash with project config
 def read_project_file
   return YAML.load_file('project.yaml')
-end
-
-# Fetches pom.xml from maven repository for given dep_uri
-# Returns pom xml object
-# Params:
-# +dep_uri+:: dependency uri from project.yaml dep entry
-def fetch_pom(dep_uri)
-# TODO заюзать faraday connection object
-# см. https://github.com/lostisland/faraday
-  res = Faraday.get $pom_filepath_prefix + dep_uri
-  return Nokogiri::XML(res.body) do |config|
-    config.noblanks
-  end
 end
 
 # Converts package.yml dep string to filepath for maven repository
@@ -49,10 +27,6 @@ def dep_to_filepath(dep_uri)
   return "#{groupId}/#{artifactId}/#{version}/#{artifactId}-#{version}.pom"
 end
 
-# Parses maven pom file and returns properties as ruby hash
-def get_pom_properties(pom_doc)
-  return pom_doc.css('properties').children.map {|c| [c.name, c.text]}.to_h
-end
 
 # Rejects all dependencies with test scope
 # from dependency nodes list
@@ -62,54 +36,16 @@ def reject_test_deps(dep_nodes)
   return dep_nodes.reject {|d| d.css('scope').text == 'test'}
 end
 
-# Substitutes given placeholder string for maven property.
-# Params:
-# +text+:: string with placeholder
-# +props+:: hash with properties from pom
-def resolve_pom_property(text, props)
-  if text.include? '$' and match = $pom_param_pattern.match(text)
-    key, _ = match.captures
-    return props.fetch(key)
-  end
-
-  return text
-end
-
-# Parses pom node and returns list of dependencies
-# Params:
-# +pom_doc+:: mvn pom root node
-def get_pom_dependecies(pom_doc)
-  props = get_pom_properties pom_doc
-  deps = reject_test_deps pom_doc.css('dependencies//dependency')
-
-  return deps.map do |dep|
-    groupId = dep.at('groupId').text
-    artifactId = dep.at('artifactId').text
-    version = resolve_pom_property dep.at('version').text, props
-
-    "#{groupId}:#{artifactId}@#{version}"
-  end
-end
-
-def find_lates(dep_uri)
-end
-
 def collect_deps(dep_uri)
   puts dep_uri
   # puts dep_to_filepath dep_uri
 end
-
 
 # 1. Резолвим все зависимости указанные в project.yml
 project_deps = read_project_file['deps'].map do |dep|
   puts dep
   dep
 end
-
-
-
-
-
 
 puts project_deps
 # pom = fetch_pom dep_to_filepath project_deps
--- src/mvn.rb
+++ src/mvn.rb
@@ -18,23 +18,77 @@ module MVN
   end
 
   # Finds latest version for dependency with provided
-  # groupdId and artifactiId
+  # groupId and artifactiId
   # Params:
   # +groupId+:: dependency groupId
   # +artifactId+:: dependency artifactId
-  def self.get_latest_version(groupId, artifactId)
+  def self.find_latest_version(groupId, artifactId)
     res = @@srch_conn.get @@select_uri % [groupId, artifactId]
     res_json = JSON::parse(res.body).fetch('response')
     return res_json.fetch('docs')[0].fetch('latestVersion')
   end
 
   class POM
+    @@param_pattern = /^\$\{(.+)\}$/
+
+    attr_reader :props, :deps
+
     def initialize(pom_xml)
-      puts 'init ' + pom_xml
-      # Nokogiri::XML(res.body) {|config| config.noblanks}
+      pom_doc = Nokogiri::XML(pom_xml) {|config| config.noblanks}
+      @props = parse_props pom_doc
+      @deps = parse_deps pom_doc
+    end
+
+    private
+      # Parses maven pom root node and returns properties as ruby hash
+      def parse_props(pom_doc)
+        return pom_doc.css('properties').children.map {|c| [c.name, c.text]}.to_h
+      end
+
+      # Parses pom node and returns list of all dependencies
+      # in "groupdId:artifactId@version" format
+      # Params:
+      # +pom_doc+:: mvn pom root node
+      def parse_deps(pom_doc)
+        return pom_doc.css('dependencies//dependency').map do |dep|
+          groupId = dep.at('groupId').text
+          artifactId = dep.at('artifactId').text
+          version = resolve_property dep.at('version').text
+          # TODO парсить текст scope или класть nil
+          scope = dep.at('scope')
+          # TODO парсить текст optional или класть false
+          optional = dep.at('optional')
+
+          Dependency.new(groupId, artifactId, version, scope, optional)
+        end
+      end
+
+      # Substitutes given placeholder string for maven property.
+      # Params:
+      # +text+:: string with placeholder
+      # +props+:: hash with properties from pom
+      def resolve_property(text)
+        if text.include? '$' and match = @@param_pattern.match(text)
+          key, _ = match.captures
+          return @props.fetch(key)
+        end
+
+        return text
+      end
+  end
+
+  class Dependency
+    attr_reader :groupdId, :artifactId, :version, :scope, :optional
+
+    def initialize(groupId, artifactId, version, scope, optional)
+      @groupId = groupId
+      @artifactId = artifactId
+      @version = version
+      @scope = scope
+      @optional = optional
     end
   end
 end
 
 # MVN::fetch_pom('com/sparkjava/spark-core/2.7.2/spark-core-2.7.2.pom')
-# MVN::get_latest_version('javax.servlet', 'javax.servlet-api')
+# MVN::find_latest_version('javax.servlet', 'javax.servlet-api')
